fd1a3a6d8755b6015b324b1ec4dc9d523517d992
Nat Welch <nat@natwelch.com>
2013-07-30 00:26:42 -0700

    fix validation for rails 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :name, :uniqueness => true, :presence => true, :format => { :with => /\A[0-9a-zA-Z]+\z/, :message => "Only numbers and letters allowed." }
+  validates :email, :email => true, :allow_nil => true
 end
