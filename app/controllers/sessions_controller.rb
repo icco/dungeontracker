546c1e2ef0932a021671c68ced2c36a0ac79d0fc
Nat Welch <nat@natwelch.com>
2013-07-30 14:24:15 -0700

    base auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    self.current_user = User.login(params['auth_key'], request['password'])
+
+    if self.current_user
+      redirect_to '/', notice: "Logged in."
+    else
+      failure
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/', notice: "Signed out!"
+  end
+
+  def failure
+    redirect_to '/', alert: "Authentication failed, please try again."
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
   validates :name, :uniqueness => true, :presence => true, :format => { :with => /\A[0-9a-zA-Z]+\z/, :message => "Only numbers and letters allowed." }
   validates :email, :email => true, :allow_nil => true
+
+  def User.login email, password
+    identity = Identity.authenticate(email, password)
+
+    if identity
+      user = User.find_or_create_by_email email
+      p user.errors.messages if !user.valid?
+
+      return user
+    else
+      return nil
+    end
+  end
 end
