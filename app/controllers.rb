996933dff01422252609e15ed0507ef5c53997eb
Nat Welch <nat@natwelch.com>
2014-03-21 11:54:08 -0700

    ...

--- app/controllers.rb
+++ app/controllers.rb
@@ -4,4 +4,12 @@ DungeonTracker::App.controllers  do
   get :index do
     render :index
   end
+
+  post :login do
+    # blah
+  end
+
+  get :new_character do
+    render :new_character
+  end
 end
