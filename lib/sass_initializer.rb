96d20869c63ee87293b393708aad8504d1c5a717
Nat Welch <nat@natwelch.com>
2014-03-21 10:58:56 -0700

    better sass options

--- lib/sass_initializer.rb
+++ lib/sass_initializer.rb
@@ -4,8 +4,12 @@ module SassInitializer
     # See http://nex-3.com/posts/88-sass-supports-rack for more details.
     # Store SASS files (by default) within 'app/stylesheets'.
     require 'sass/plugin/rack'
-    Sass::Plugin.options[:template_location] = Padrino.root("app/stylesheets")
-    Sass::Plugin.options[:css_location] = Padrino.root("public/stylesheets")
+    Sass::Plugin.options[:always_update]     = (Padrino.env == :development)
+    Sass::Plugin.options[:css_location]      = Padrino.root("public/css")
+    Sass::Plugin.options[:full_exception]    = (Padrino.env == :development)
+    Sass::Plugin.options[:never_update]      = (Padrino.env == :production)
+    Sass::Plugin.options[:style]             = :compact
+    Sass::Plugin.options[:template_location] = Padrino.root("app/css")
     app.use Sass::Plugin::Rack
   end
 end
