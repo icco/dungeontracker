9aeaad3dd08310d9a3c5d43ef5be904fc8fac32d
Nat Welch <nat@natwelch.com>
2014-03-24 15:25:48 -0700

    character sheet.

---
+++ public/js/application.js
@@ -0,0 +1,13 @@
+function addElement(element) {
+  section = $(element).parents('div.form_section')[0];
+  to_copy = $(element).parent('span')[0];
+  clone = $(to_copy).clone();
+  $(to_copy).children('.form_add').empty();
+  console.log(section)
+  $(section).append(clone);
+}
+
+function deleteElement(element){
+  to_del = $(element).parent('span')[0];
+  $(to_del).empty();
+}
